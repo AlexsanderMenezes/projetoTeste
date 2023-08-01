ec86aba425c59fe0a894f901233dc42c0a5aa242
alexsanderMenezes <alexsander.silva.menezes@gmail.com>
2023-07-31 21:53:35 -0300

    Ajustes

--- ProjetoPadraoDotnetCore/Infraestrutura.Data/Migrations/20230801005226_addFks.Designer.cs
+++ ProjetoPadraoDotnetCore/Infraestrutura.Data/Migrations/20230801005226_addFks.Designer.cs
@@ -4,14 +4,16 @@ using Infraestrutura.DataBaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infraestrutura.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230801005226_addFks")]
+    partial class addFks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProjetoPadraoDotnetCore/Infraestrutura.Data/Migrations/20230801005226_addFks.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infraestrutura.Migrations
+{
+    public partial class addFks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_IdUsuarioCadastro",
+                table: "Usuario",
+                column: "IdUsuarioCadastro");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SkillUsuario_IdUsuario",
+                table: "SkillUsuario",
+                column: "IdUsuario");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SkillUsuario_Usuario_IdUsuario",
+                table: "SkillUsuario",
+                column: "IdUsuario",
+                principalTable: "Usuario",
+                principalColumn: "IdUsuario",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Usuario_Usuario_IdUsuarioCadastro",
+                table: "Usuario",
+                column: "IdUsuarioCadastro",
+                principalTable: "Usuario",
+                principalColumn: "IdUsuario",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SkillUsuario_Usuario_IdUsuario",
+                table: "SkillUsuario");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Usuario_Usuario_IdUsuarioCadastro",
+                table: "Usuario");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Usuario_IdUsuarioCadastro",
+                table: "Usuario");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SkillUsuario_IdUsuario",
+                table: "SkillUsuario");
+        }
+    }
+}
